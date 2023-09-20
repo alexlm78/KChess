60004c9a1bd08dc3cf65774072758786b8d96405
Alejandro Lopez Monzon <alexlm78@gmail.com>
2023-09-20 16:01:08 -0600

    Drawing the board and pieces on board.

--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -1,25 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ChessLogic;
 
-namespace ChessUI {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
+namespace ChessUI;
+
+public partial class MainWindow : Window {
+
+    private readonly Image[,] pieceImages = new Image[8, 8];
+
+    private GameState gamesState;
+
+    public MainWindow() {
+        InitializeComponent();
+        InitializeBoard();
+
+        gamesState = new GameState(Player.White, Board.Initial());
+        DrawBoard(gamesState.Board);
+    }
+
+    private void InitializeBoard() {
+        for(int r =0; r < 8; r++) {
+            for(int c =0; c < 8; c++) {
+                Image image = new Image();
+                pieceImages[r, c] = image;
+                PieceGrid.Children.Add(image);
+            }
         }
+    }
+
+    private void DrawBoard(Board board) {
+        for(int r =0; r < 8; r++) 
+            for(int c =0; c < 8; c++) 
+                pieceImages[r, c].Source = Images.GetImage(board[r, c]);
     }
 }
