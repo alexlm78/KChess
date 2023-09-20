4b1163e84e0b45df1e07be1f3dfc6834b49e07a0
Alejandro Lopez Monzon <alexlm78@gmail.com>
2023-09-20 15:58:58 -0600

    Set Images for every piece

---
+++ ChessUI/Images.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using ChessLogic;
+
+namespace ChessUI;
+
+public static class Images {
+
+    private static readonly Dictionary<PieceType, ImageSource> whiteSources = new() {
+        {PieceType.Pawn, LoadImage("Assets/WhitePawn.png")},
+        {PieceType.Rook, LoadImage("Assets/WhiteRook.png")},
+        {PieceType.Knight, LoadImage("Assets/WhiteKnight.png")},
+        {PieceType.Bishop, LoadImage("Assets/WhiteBishop.png")},
+        {PieceType.Queen, LoadImage("Assets/WhiteQueen.png")},
+        {PieceType.King, LoadImage("Assets/WhiteKing.png")}
+    };
+
+    private static readonly Dictionary<PieceType, ImageSource> blackSources = new() {
+        {PieceType.Pawn, LoadImage("Assets/BlackPawn.png")},
+        {PieceType.Rook, LoadImage("Assets/BlackRook.png")},
+        {PieceType.Knight, LoadImage("Assets/BlackKnight.png")},
+        {PieceType.Bishop, LoadImage("Assets/BlackBishop.png")},
+        {PieceType.Queen, LoadImage("Assets/BlackQueen.png")},
+        {PieceType.King, LoadImage("Assets/BlackKing.png")}
+    };
+
+    private static ImageSource LoadImage(string filePath) {
+        return new BitmapImage(new Uri(filePath, UriKind.Relative));
+    }
+
+    public static ImageSource GetImage(Player color, PieceType type) {
+        return color switch {
+            Player.White => whiteSources[type],
+            Player.Black => blackSources[type],
+            _ => null
+        };
+    }
+
+    public static ImageSource GetImage(Piece piece) {
+        return (piece==null) ? null : GetImage(piece.Color, piece.Type);
+    }
+}
