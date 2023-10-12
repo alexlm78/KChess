66324d351c257118a48cdb8282b39556827740b9
Alejandro Lopez Monzon <alexlm78@gmail.com>
2023-10-12 12:23:35 -0600

    add: Checkmate and stealmate states for game.

--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -3,6 +3,8 @@
 public class GameState {
     public Board Board { get; }
     public Player CurrentPlayer { get; private set; }
+    public Result Result { get; private set; } = null;
+
     /*public bool IsCheck { get; }
     public bool IsCheckmate { get; }
     public bool IsStalemate { get; }
@@ -25,6 +27,28 @@ public class GameState {
     public void MakeMove(Move move) {
         move.Execute(Board);
         CurrentPlayer = CurrentPlayer.Opponent();
+        CheckForGameOver();
+    }
+
+    public IEnumerable<Move> AllLegalMovesFor(Player player) {
+        IEnumerable<Move> moveCandidates = Board.PiecePositionsFor(player).SelectMany(pos => {
+            Piece piece = Board[pos];
+            return piece.GetMoves(pos, Board);
+        });
+        return moveCandidates.Where(move => move.IsLegal(Board));
+    }
+
+    private void CheckForGameOver() {
+        if(!AllLegalMovesFor(CurrentPlayer).Any()) {
+            if(Board.IsInCheck(CurrentPlayer)) 
+                Result = Result.Win(CurrentPlayer.Opponent());
+            else 
+                Result = Result.Draw(EndReason.Stalemate);
+        }
+    }
+
+    public bool IsGameOver() {
+        return Result != null;
     }
 
     /*public GameState(Board board, Player player, bool isCheck, bool isCheckmate, bool isStalemate, bool isDraw) {
---
+++ ChessLogic/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic; 
+internal class Result {
+    public Player Winner { get; }
+    public EndReason Reason { get; }
+
+    public Result(Player winner, EndReason reason) {
+        Winner = winner;
+        Reason = reason;
+    }
+
+    public static Result Win(Player winner) {
+        return new Result(winner, EndReason.Checkmate);
+    }
+
+    public static Result Draw(EndReason reason) {
+        return new Result(Player.None, reason);
+    }
+}
